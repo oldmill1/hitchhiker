2340994fed67aa12adc18c1f6865da1c1a7ecf88
Ankur Taxali <ataxali@gmail.com>
2025-11-08 19:25:03 -0500

    Fix the styles in the sidebar

--- src/lib/components/shared/List/List.svelte
+++ src/lib/components/shared/List/List.svelte
@@ -19,22 +19,18 @@
 <ul class={styles.container}>
   {#each items as item}
     <li>
+      <span class={styles.emojiSpace}>
+        {#if item.starred}⭐ {/if}
+        {#if item.selected}⚪ {/if}
+      </span>
       {#if item.link}
         <a href={item.link}>
           <Text weight="md">
-            <span class={styles.emojiSpace}>
-              {#if item.starred}⭐ {/if}
-              {#if item.selected}⚪ {/if}
-            </span>
             {item.content}
           </Text>
         </a>
       {:else}
         <Text>
-          <span class={styles.emojiSpace}>
-            {#if item.starred}⭐ {/if}
-            {#if item.selected}⚪ {/if}
-          </span>
           {item.content}
         </Text>
       {/if}
